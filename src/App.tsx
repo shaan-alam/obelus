c236b145cab84922b2813cf0733defc601941c0d
Yashwant Jodha <ysjodha1@gmail.com>
2022-12-18 20:51:27 +0530

    add styles

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,27 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <div className="container mx-auto my-8">
-        <div className="flex items-center justify-between">
-          <input type="text" placeholder="First Name" className="py-3 px-4 rounded-md focus:ring outline-none border" />
-          <input type="text" placeholder="Last Name" className="py-3 px-4 rounded-md focus:ring outline-none border" />
-          <input type="text" placeholder="Email" className="py-3 px-4 rounded-md focus:ring outline-none border" />
-          <input type="text" placeholder="Address" className="py-3 px-4 rounded-md focus:ring outline-none border" />
+      <div className="container md:mx-auto my-8">
+        <h1 className="text-4xl my-10 font-medium">Database Search</h1>
+        <div className="flex justify-between flex-col md:flex-row md:flex-wrap mx-8 md:mx-0">
+          <div className="md:basis-2/5">
+            <label className="block text-left font-medium my-2">First Name</label>
+            <input type="text" placeholder="Noah" className="shadow py-3 px-4 w-full rounded-md focus:ring outline-none border" />
+          </div>
+          <div className="mt-5 md:basis-2/5 md:mt-0">
+            <label className="block text-left font-medium my-2">Last Name</label>
+            <input type="text" placeholder="Smith" className="shadow py-3 px-4 w-full rounded-md focus:ring outline-none border" />
+          </div>
+          <div className="mt-5 md:basis-2/5">
+            <label className="block text-left font-medium my-2">Email</label>
+            <input type="text" placeholder="abc@xyz.com" className="shadow py-3 px-4 w-full rounded-md focus:ring outline-none border" />
+          </div>
+          <div className="mt-5 md:basis-2/5">
+            <label className="block text-left font-medium my-2">LinkedIn Username</label>
+            <input type="text" placeholder="noah123" className="shadow py-3 px-4 w-full rounded-md focus:ring outline-none border" />
+          </div>     
         </div>
-        <button className="bg-blue-700 font-bold text-white w-full rounded-md py-3 my-4 active:ring">Search</button>
+        <button className="shadow bg-blue-200 mt-16 md:float-right font-bold text-blue-900 w-2/3 md:w-48 rounded-md py-2.5 my-7 active:ring">Search</button>
       </div>
     </div>
   );
