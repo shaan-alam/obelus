d57188822ba2cf32e13c5ae23525f2c225895115
shaan-alam <shaanalam369@gmail.com>
2022-12-18 17:56:16 +0530

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container mx-auto my-8">
+        <div className="flex items-center justify-between">
+          <input type="text" placeholder="First Name" className="py-3 px-4 rounded-md focus:ring outline-none border" />
+          <input type="text" placeholder="Last Name" className="py-3 px-4 rounded-md focus:ring outline-none border" />
+          <input type="text" placeholder="Email" className="py-3 px-4 rounded-md focus:ring outline-none border" />
+          <input type="text" placeholder="Address" className="py-3 px-4 rounded-md focus:ring outline-none border" />
+        </div>
+        <button className="bg-blue-700 font-bold text-white w-full rounded-md py-3 my-4 active:ring">Search</button>
+      </div>
     </div>
   );
 }
