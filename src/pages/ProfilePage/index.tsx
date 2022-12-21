699d4e6f87c2766142d76a3c6bf8b651b35d275d
shaan-alam <shaanalam369@gmail.com>
2022-12-21 20:36:28 +0530

    added loading sign

--- src/pages/ProfilePage/index.tsx
+++ src/pages/ProfilePage/index.tsx
@@ -23,7 +23,11 @@ const ProfilePage = () => {
     }
   );
 
-  return (
+  return isLoading || isFetching ? (
+    <section className="h-screen w-full flex items-center justify-center">
+      <h1 className="text-3xl">LOADING</h1>
+    </section>
+  ) : (
     <section>
       <div className="w-[70%] mx-auto">
         <div className="block_content my-8 px-8 rounded-md">
@@ -69,19 +73,48 @@ const ProfilePage = () => {
 
         <div className="block_content my-4 px-8 rounded-md">
           <h1 className="text-gray-800 font-bold text-2xl my-4">Job Details</h1>
-          <div className="text-gray-600 mb-2">Industry: {getValueOf(lead?.data.industry)}</div>
-          <div className="text-gray-600 mb-2">Job Title: {getValueOf(lead?.data.job_title)}</div>
-          <div className="text-gray-600 mb-2">Job Title Role: {getValueOf(lead?.data.job_title_role)}</div>
-          <div className="text-gray-600 mb-2">Job Company ID: {getValueOf(lead?.data.job_company_id)}</div>
-          <div className="text-gray-600 mb-2">Job Company Name: {getValueOf(lead?.data.job_company_name)}</div>
-          <div className="text-gray-600 mb-2">Job Company Website: {getValueOf(lead?.data.job_company_website)}</div>
-          <div className="text-gray-600 mb-2">Job Company Size: {getValueOf(lead?.data.job_company_size)}</div>
-          <div className="text-gray-600 mb-2">Job Company Founded: {getValueOf(lead?.data.job_company_founded)}</div>
-          <div className="text-gray-600 mb-2">Job Company Industry: {getValueOf(lead?.data.job_company_industry)}</div>
-          <div className="text-gray-600 mb-2">Job Company LinkedIn URL: {getValueOf(lead?.data.job_company_linkedin_url)}</div>
-          <div className="text-gray-600 mb-2">Job Company Location Name: {getValueOf(lead?.data.job_company_location_name)}</div>
-          <div className="text-gray-600 mb-2">Job Company Location country: {getValueOf(lead?.data.job_company_location_country)}</div>
-          <div className="text-gray-600 mb-2">Job Company Summary: {getValueOf(lead?.data.job_summary)}</div>
+          <div className="text-gray-600 mb-2">
+            Industry: {getValueOf(lead?.data.industry)}
+          </div>
+          <div className="text-gray-600 mb-2">
+            Job Title: {getValueOf(lead?.data.job_title)}
+          </div>
+          <div className="text-gray-600 mb-2">
+            Job Title Role: {getValueOf(lead?.data.job_title_role)}
+          </div>
+          <div className="text-gray-600 mb-2">
+            Job Company ID: {getValueOf(lead?.data.job_company_id)}
+          </div>
+          <div className="text-gray-600 mb-2">
+            Job Company Name: {getValueOf(lead?.data.job_company_name)}
+          </div>
+          <div className="text-gray-600 mb-2">
+            Job Company Website: {getValueOf(lead?.data.job_company_website)}
+          </div>
+          <div className="text-gray-600 mb-2">
+            Job Company Size: {getValueOf(lead?.data.job_company_size)}
+          </div>
+          <div className="text-gray-600 mb-2">
+            Job Company Founded: {getValueOf(lead?.data.job_company_founded)}
+          </div>
+          <div className="text-gray-600 mb-2">
+            Job Company Industry: {getValueOf(lead?.data.job_company_industry)}
+          </div>
+          <div className="text-gray-600 mb-2">
+            Job Company LinkedIn URL:{" "}
+            {getValueOf(lead?.data.job_company_linkedin_url)}
+          </div>
+          <div className="text-gray-600 mb-2">
+            Job Company Location Name:{" "}
+            {getValueOf(lead?.data.job_company_location_name)}
+          </div>
+          <div className="text-gray-600 mb-2">
+            Job Company Location country:{" "}
+            {getValueOf(lead?.data.job_company_location_country)}
+          </div>
+          <div className="text-gray-600 mb-2">
+            Job Company Summary: {getValueOf(lead?.data.job_summary)}
+          </div>
         </div>
       </div>
     </section>
